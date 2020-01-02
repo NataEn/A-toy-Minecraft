7a4ba09fbf6918ee0aa6bf888428f31fbc61240d
james <jjbe.93@gmail.com>
2020-01-02 08:55:21 +0200

    added sizes for minecraft canvas and button functionalities

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
-let Minecraft = {};
+const Minecraft = {};
 
 Minecraft.canvas = [];
 Minecraft.canvasWidth = 1000;
 
 Minecraft.start = function(){
-    
+    Minecraft.buttons();
+    Minecraft.fillCanvas();
 }
 
+Minecraft.fillCanvas = function(){
+    $('.cart-container').css({"width":"200px","height":"100vh","background-color": "sandybrown"});
+    $('.minecraft-field').css({"width":`${Minecraft.canvasWidth}px`,"height":"100vh","background-color":"skyblue","float":"left","overflow":"scroll"})
+    
+    $('.tools-container').css({"width":"200px","height":"100vh","background-color": "sandybrown"})
+    let block = $('div');
+
+}
 Minecraft.buttons = function () {
     $('.game-size .btn').click(function () {
+        console.log("clicked");
         Minecraft.canvasWidth = $(this).val();
         $(".selected").removeClass('selected');
         $(this).addClass('selected');
+        console.log(Minecraft.canvasWidth);
     })
+   
 }
 $(document).ready(function () {
     $('.start-btn').click(function () {
         $('.welcome').fadeOut("slow");
+        Minecraft.start();
     })
 });
+Minecraft.start();
