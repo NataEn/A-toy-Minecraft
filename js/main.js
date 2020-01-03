36343b2ad798e7b0df49eeb4b74833e82ea2c27c
james <jjbe.93@gmail.com>
2020-01-03 15:38:08 +0200

    fixed grid still working on it

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 const Minecraft = {};
 
 Minecraft.canvas = [];
-Minecraft.canvasWidth = 1000;
+// Minecraft.canvasWidth = 1000;
 
-Minecraft.start = function(){
+Minecraft.start = function () {
     Minecraft.buttons();
     Minecraft.fillCanvas();
 }
 
-Minecraft.fillCanvas = function(){
-    $('.cart-container').css({"width":"200px","height":"100vh","background-color": "sandybrown"});
-    $('.minecraft-field').css({"width":`${Minecraft.canvasWidth}px`,"height":"100vh","background-color":"skyblue","float":"left","overflow":"scroll"})
-    
-    $('.tools-container').css({"width":"200px","height":"100vh","background-color": "sandybrown"})
-    let block = $('div');
+Minecraft.fillCanvas = function () {
 
+    let field = document.getElementById('minecraft-field');
+    let row = new Array(20);
+    for(let i = 0; i < row.length; i++){
+        row[i] = new Array(13);
+    }
+    for (let i = 0; i < row.length; i++) {
+        let temp = document.createElement('div'); 
+        temp.className = "block";
+        for (let j = 0; j < row[i].length; j++) { 
+             row[i][j] = document.createElement('div');
+             row[i][j].classList.add('block');
+             row[i][j].innerText = `row ${j} col ${i} `;
+             
+             temp.appendChild(row[i][j]);
+        }
+        field.appendChild(temp);
+    }
+   
+    
+     
+    
 }
 Minecraft.buttons = function () {
     $('.game-size .btn').click(function () {
@@ -24,12 +40,11 @@ Minecraft.buttons = function () {
         $(this).addClass('selected');
         console.log(Minecraft.canvasWidth);
     })
-   
 }
 $(document).ready(function () {
     $('.start-btn').click(function () {
         $('.welcome').fadeOut("slow");
-        Minecraft.start();
+        //Minecraft.start();
     })
 });
 Minecraft.start();
